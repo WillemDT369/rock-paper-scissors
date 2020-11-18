8b11e0724c630506c9f04f1dcabb565a266e710d
WillemDT369 <detroyerwillem@hotmail.com>
2020-11-18 19:09:05 +0100

    Add variables for choices and for getting the value of the button pushed

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,18 @@
 
+let userScore = 0;
+let computerScore = 0;
+
+let choices = ['rock', 'paper', 'scissor'];
+
+let userRock = document.getElementById('rock').getAttribute('value');
+let userPaper = document.getElementById('paper').getAttribute('value');
+let userScissors = document.getElementById('scissors').getAttribute('value');
+
+
+
+
+
+
+
+
+console.log(userPaper);
